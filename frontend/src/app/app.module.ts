678b1c6aabab1dc5693c15bce9ce42d17ec6002e
David Silwal <david.silwal@ekbana.info>
2020-05-15 00:08:55 +0545

    specify url

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { WeatherForecastClient } from './weatherforecast.client';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WeatherForecastClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
